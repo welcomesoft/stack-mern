60dca1f2338486f154874cdc6963f358a65537f0
Darío Welcome <welcomedario9@gmail.com>
2020-11-12 01:15:23 -0600

    Conexion a la base de datos

---
+++ backend/src/database.js
@@ -0,0 +1,21 @@
+// requiere el modulo de conexion a mongodb
+const mongoose = require("mongoose");
+
+// valida si fue definida URI sino define una de pruebas.
+const URI = process.env.MONGODB_URI 
+    ? process.env.MONGODB_URI 
+    : 'mongodb://localhost/testdb';
+
+    const PORT = process.env.MONGODB_PORT;
+
+// archivo de configuracion
+const config = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+}; 
+
+mongoose.connect(URI, config)
+.then(db => console.log('Database connected on port', PORT))
+.catch(err => console.error(err));
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
+// requiere las variables de entorno antes que inicie el servidor
+require('dotenv').config();
+
 // importa el servidor desde app
 const app = require('./app');
+require('./database');
 
 // define una variable y le pasa un puerto para nuesto servidor
 app.set('PORT', 4000);
@@ -11,8 +15,8 @@ app.set('PORT', 4000);
  * en caso de no haber uno, le pasa el puerto en una variable.
  */
 async function main() {
-    await app.listen(process.env.Port || app.get('PORT'));
-    console.log('server on port', app.get('PORT'));
+    await app.listen(process.env.PORT || app.get('PORT'));
+    console.log('Server running on port', app.get('PORT'));
 }
 
 // llama la funcion main

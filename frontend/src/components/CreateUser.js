4ca79c456699c7f25de517041f0008c954fbf405
Darío Welcome <welcomedario9@gmail.com>
2020-11-27 21:58:54 -0600

    Configuracion de create users & HTTP, POST, Delete

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -4,26 +4,63 @@ import axios from "axios";
 export default class CreateUser extends Component {
   state = {
     users: [],
-    username: '',
+    username: "",
   };
 
   // muestra los datos una vez que el componente ha sido montado
   async componentDidMount() {
-    const res = await axios.get("http://localhost:4000/api/users");
-    this.setState({ users: res.data });
+    this.updateList();
   }
 
+  // verifica cada actualizacion en el
   onChangeUsername = (e) => {
-    this.setState({username: e.target.value});
+    this.setState({ username: e.target.value });
+  };
+
+  // actualiza la lista
+  updateList = async () => {
+    const res = await axios.get("http://localhost:4000/api/users");
+    this.setState({ users: res.data });
+  };
+
+  fieldClean() {
+    this.setState({ username: "" });
   }
+
+  onSubmit = async (e) => {
+    // evita que se refresque el navegador
+    e.preventDefault();
+    // hace la peticion post al backend
+    await axios.post("http://localhost:4000/api/users", {
+      username: this.state.username,
+    });
+    // actualiza la lista
+    this.updateList();
+    // limpia el campo username
+    this.fieldClean();
+  };
+
+  deleteUser = async (id) => {
+    await axios.delete("http://localhost:4000/api/users/" + id);
+    this.updateList();
+  };
 
   render() {
     return (
       <div className="row">
         <div className="col-md-4">
           <div className="card card-body">
-            <h4 className="card-title text-dark">
-              Create new user &nbsp;
+            <h4 className="card-title text-dark">Create new user</h4>
+            <form onSubmit={this.onSubmit}>
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+              />
+            </form>
+            <button type="submit" className="btn btn-primary mt-2">
+              Save &nbsp;
               <svg
                 width="1em"
                 height="1em"
@@ -37,14 +74,7 @@ export default class CreateUser extends Component {
                   d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
                 />
               </svg>
-            </h4>
-            <form>
-              <input
-                type="text"
-                className="form-control"
-                onChange={this.onChangeUsername}
-              />
-            </form>
+            </button>
           </div>
         </div>
         <div className="col-md-8">
@@ -53,6 +83,7 @@ export default class CreateUser extends Component {
               <li
                 className="list-group-item list-group-item-action"
                 key={user._id.toString()}
+                onDoubleClick={() => this.deleteUser(user._id)}
               >
                 <svg
                   width="1em"
